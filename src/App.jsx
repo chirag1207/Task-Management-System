b139e3ef7a9c408cdf3fca38d417339578650bfc
chirag1207 <chiragnarkar2507@gmail.com>
2026-03-10 01:44:36 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,57 @@
 import Login from './pages/Login'
-import{ Router, Routes, Route, Navigate,Outlet, useLocation } from 'react-router-dom';
+import './index.css'
+import { Router, Routes, Route, Navigate, Outlet, useLocation } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Tasks from './pages/Tasks';
 import TaskDetails from './pages/TaskDetails';
 import Users from './pages/Users';
-import Trash from './pages/Trash';  
+import Trash from './pages/Trash';
+import { Toaster } from 'sonner';
+
+function Layout() {
+  const location = useLocation();
+  return user ? (
+    <div className="w-full min-h-screen flex flex-col md:flex-row">
+      <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
+        {/* {/<sidebar/>/} */}
+      </div>
+      {/* {<MobileSidebar/>} */}
+      <div className="flex-1 overflow-y-auto">
+        {/* {<Navbar/>} */}
+      </div>
+      <div className="p-4 2x1:px-10">
+        <Outlet />
+      </div>
+    </div>
+  ) : (
+    <Navigate to='/log-in' state={{ from: location }} replace />
+  )
+}
 
 function App() {
-  
+
   return (
-    <main className = 'w-full min-h-screen bg-[#f3f4f6]'>
+    <main className='w-full min-h-screen bg-[#f3f4f6]'>
+ 
+
       <Routes>
-        <route elementr ={<Layout/>}>
-          <Route path = '/' element={<Navigate to = '/dashboard'/>}/>
-          <Route path = '/dashboard' element={<Dashboard/>}/>
-          <Route path = '/tasks' element={<Tasks/>}/>
-          <Route path = '/completed/:staus' element={<TaskDetails/>}/>
-          <Route path = '/users' element={<Users/>}/>
-          <Route path = '/trash' element={<Trash/>}/>
-        </route>
+        <Route element={<Layout />}>
+          <Route path='/' element={<Navigate to='/dashboard' />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/tasks' element={<Tasks />} />
+          <Route path='/completed/:status' element={<Tasks />} />
+          <Route path='/in-progress/:status' element={<Tasks />} />
+          <Route path='/todo/:status' element={<Tasks />} />
+          <Route path='/users' element={<Users />} />
+          <Route path='/trash' element={<Trash />} />
+          <Route path='/task/:id' element={<TaskDetails />} />
+        </Route>
+        <Route path='/log-in' element={<Login />} />
       </Routes>
+      <Toaster richColors />
     </main>
   )
-  
+
 }
 
 export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    const user="";  
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        user && navigate('/dashboard');
+    }, [user, navigate]);
+    return (
+        <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex:row bg-[#f3f4f6]">
+            <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center">
+                <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
+                    <div className="w-full md:max-w-lg 2x1:max-w-3x1 flex flex-col items-center justify-center gap-5 md:gap-y-10 2x1:-mt-20">
+                        <span className="flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300 text-gray-600">Manage all your atask at once place</span>
+                        <p className ="flex flex-col gap-0 md:gap-4 text-4x1 md:text-6xl 2xl:text-7xl font-black text-center text-blue-700">
+                            <span>Cloud-based</span>
+                            <span>Task Manager</span>    
+                        </p>
+
+                    </div>
+
+                    <div className='cell'>
+                        <div className ="circle rotate-in-up-left"></div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default Login
+
+export default Login;
